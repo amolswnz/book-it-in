fefde5adb661936edffb1bb5e927f429a8d32077
Amol Wankhede <amolsw.nz@gmail.com>
2017-07-28 14:14:43 +1200

    Before changing layout

--- public/app/app.js
+++ public/app/app.js
@@ -3,11 +3,19 @@
   angular.module('bookitinApp', ['ngResource', 'ngRoute']);
 
   angular.module('bookitinApp')
+    .run(['$rootScope', '$location', '$routeParams', function($rootScope, $location, $routeParams) {
+      $rootScope.$on('$routeChangeSuccess', function(e, current, pre) {
+        console.log('Current route name: ' + $location.path());
+        console.log($routeParams);
+      });
+    }])
+
     .config(function($routeProvider, $locationProvider) {
-      // $locationProvider.html5Mode({
-      //     enabled: true,
-      //     requireBase: false
-      // });
+      $locationProvider.html5Mode({
+          enabled: true,
+          requireBase: false
+      });
+      // $locationProvider.html5Mode(true);
 
       // /partials = /public/app defined in express.js
       console.log('- inside angular module bookitinApp before routing');
@@ -19,37 +27,37 @@
           controller: 'HomeCtrl'
         })
         .when('/login', {
-          templateUrl: '/partials/user/login',
+          templateUrl: '/partials/user/login2',
           controller: 'UserCtrl'
         });
-        // .when('/home', {
-        //   templateUrl: '/partials/main/home',
-        //   controller: 'HomeCtrl'
-        // })
-        // .when('/user', {
-        //   templateUrl: '/partials/user/user-all',
-        //   controller: 'AllUserCtrl',
-        //   controllerAs: 'users'
-        // })
-        // .when('/user/add', {
-        //   templateUrl: '/partials/user/user-add',
-        //   controller: 'UserAddCtrl',
-        //   controllerAs: 'addCtrl'
-        // })
-        // .when('/user/:id', {
-        //   templateUrl: '/partials/user/user-profile',
-        //   controller: 'UserProfileCtrl',
-        //   controllerAs: 'profileCtrl'
-        // })
-        // .when('/user/:id/edit', {
-        //   templateUrl: '/partials/user/user-edit',
-        //   controller: 'UserProfileCtrl',
-        //   controllerAs: 'editCtrl'
-        // })
-        // .when('/user/:id/delete', {
-        //   templateUrl: '/partials/user/user-delete',
-        //   controller: 'UserProfileCtrl',
-        //   controllerAs: 'vm'
-        // });
+      // .when('/home', {
+      //   templateUrl: '/partials/main/home',
+      //   controller: 'HomeCtrl'
+      // })
+      // .when('/user', {
+      //   templateUrl: '/partials/user/user-all',
+      //   controller: 'AllUserCtrl',
+      //   controllerAs: 'users'
+      // })
+      // .when('/user/add', {
+      //   templateUrl: '/partials/user/user-add',
+      //   controller: 'UserAddCtrl',
+      //   controllerAs: 'addCtrl'
+      // })
+      // .when('/user/:id', {
+      //   templateUrl: '/partials/user/user-profile',
+      //   controller: 'UserProfileCtrl',
+      //   controllerAs: 'profileCtrl'
+      // })
+      // .when('/user/:id/edit', {
+      //   templateUrl: '/partials/user/user-edit',
+      //   controller: 'UserProfileCtrl',
+      //   controllerAs: 'editCtrl'
+      // })
+      // .when('/user/:id/delete', {
+      //   templateUrl: '/partials/user/user-delete',
+      //   controller: 'UserProfileCtrl',
+      //   controllerAs: 'vm'
+      // });
     });
 }());
